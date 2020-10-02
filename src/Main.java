178b82e387037215ca79ec1e1167cd94f6f631d7
tekkukerli <kerlitekku@gmail.com>
2020-10-02 12:56:38 +0300

    ül 10

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.println("Type a number ");
-        int firstNumber = Integer.parseInt(reader.nextLine());
-        System.out.println("Type another number ");
-        int secondNumber = Integer.parseInt(reader.nextLine());
-        double result = (double)firstNumber / secondNumber;
-        System.out.println("Division: " + firstNumber + "/" + secondNumber + "=" + result);
+        System.out.println("Type the radius ");
+        int radius = Integer.parseInt(reader.nextLine());
+        double circumference = 2 * Math.PI * radius;
+        System.out.println("Circumference of the circle: " + circumference);
     }
 }
 
