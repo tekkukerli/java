7ab3b51e78393c189508e069e14109957597d6ba
tekkukerli <kerlitekku@gmail.com>
2020-10-02 13:36:11 +0300

    ül 16

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.println("Type the radius ");
-        int radius = Integer.parseInt(reader.nextLine());
-        double circumference = 2 * Math.PI * radius;
-        System.out.println("Circumference of the circle: " + circumference);
+        System.out.println("Type a number ");
+        int number = Integer.parseInt(reader.nextLine());
+        if(number % 2 == 0) {
+            System.out.println("Number " + number + " is even.");
+        } else {
+            System.out.println("Number " + number + " is odd.");
+        }
     }
 }
 
