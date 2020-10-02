4f876d45d6112f698a202867be9bddba3f8f0c72
tekkukerli <kerlitekku@gmail.com>
2020-10-02 12:48:37 +0300

    ül 5

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Kerli Tekku");
+        int days = 365;
+        int hours = 24;
+        int minutes = 60;
+        int seconds = 60;
+        int secondsInAYear = days * hours * minutes * seconds;
+        System.out.println("There are " + secondsInAYear + " seconds in a year.");
 
     }
 }
