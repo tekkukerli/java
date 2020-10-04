3c58a284f1c309c28bd6f549f270e498020d4a38
tekkukerli <kerlitekku@gmail.com>
2020-10-04 18:38:42 +0300

    ül 19

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.println("Type the points [0-60]: ");
+        System.out.println("How old are you? ");
 
         int number = Integer.parseInt(reader.nextLine());
-        if (number <= 29) {
-            System.out.println("Failed");
-        } else if (number > 29 && number < 35) {
-            System.out.println("Grade: 1");
-        } else if (number > 34 && number < 40) {
-            System.out.println("Grade: 2");
-        } else if (number > 39 && number < 45) {
-            System.out.println("Grade: 3");
-        } else if (number > 44 && number < 50) {
-            System.out.println("Grade: 4");
+        if (number >= 0 && number < 121) {
+            System.out.println("OK");
         } else {
-            System.out.println("Grade: 5");
+            System.out.println("Impossible!");
         }
     }
 }
