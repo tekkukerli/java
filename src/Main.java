334705c2edfda0d0434991568429834076cb3291
tekkukerli <kerlitekku@gmail.com>
2020-10-09 14:42:01 +0300

    ül 26

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,22 @@
-import java.util.Scanner;
+import java.util.Scanner; //to read user input
 
 public class Main {
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        System.out.println("Type the password: ");
+        Scanner reader = new Scanner(System.in); //loeb andmed
+        int sum = 0;
+        while (true) {
+            int read = Integer.parseInt(reader.nextLine()); //loeb sisendit
+            if (read == 0) {
+                break;
+            }
 
-        String password = reader.nextLine();
-        while (!password.equals("carrot")) {
-                System.out.println("Wrong!");
-                System.out.println("Type the password: ");
-                password = reader.nextLine();
+            // DO SOMETHING HERE
+            sum = sum + read; //sum += read;
+
+            System.out.println("Sum now: " + sum);
         }
-        System.out.println("Right!");
-        System.out.println();
-        System.out.println("The secret is: jryy qbar!");
+
+        System.out.println("Sum in the end: " + sum);
     }
 }
 
