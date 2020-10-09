fd95f7255d3de281328828e6607cb48f110fcdf8
tekkukerli <kerlitekku@gmail.com>
2020-10-09 18:03:53 +0300

    ül 39

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,21 @@
 import java.util.Scanner; //to read user input
 
 public class Main {
-    private static boolean drawNumber(int guess, int number, int count) {
-
-        System.out.println(number);
-        if(guess < number) {
-            System.out.println("The number is greater, guesses made: " + count);
-            return true;
-        } else if (guess > number){
-            System.out.println("The number is lesser, guesses made: " + count);
-            return true;
-        } else {
-            System.out.println("Congratulations, your guess is correct");
-            return false;
-        }
-      }
-
     public static void main(String[] args) {
-        int number = (int)(Math.random() * 101);
-        Scanner reader = new Scanner(System.in);
-        System.out.println("Guess a number: ");
-        int guess = Integer.parseInt(reader.nextLine());
-        int count = 1;
-        while(drawNumber(guess, number, count)){
-            System.out.println("Guess a number: ");
-            guess = Integer.parseInt(reader.nextLine());
-            count++;
+        printStars(5);
+        printStars(3);
+        printStars(9);
+    }
+    private static void printStars(int amount) {
+        // you can print one star with the command
+        // System.out.print("*");
+        // call this command amount times
+        int i = 1;
+        while (i <= amount) {
+            System.out.print("*");
+            i++;
         }
+        System.out.println();
     }
 }
 
