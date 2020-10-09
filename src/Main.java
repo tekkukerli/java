613e84bb203c523602f37261c19118bd1a6b01d7
tekkukerli <kerlitekku@gmail.com>
2020-10-09 14:50:19 +0300

    ül 32

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,15 @@ import java.util.Scanner; //to read user input
 public class Main {
     public static void main(String[] args) {
        Scanner reader = new Scanner(System.in);
-        System.out.println("Up to what number");
+        System.out.println("Until what? ");
         int upperNumber = Integer.parseInt(reader.nextLine());
         int currentNumber = 1;
+        int sum = 0;
         while (currentNumber <= upperNumber){
-            System.out.println(currentNumber);
+            sum += currentNumber;
             currentNumber++;
         }
+        System.out.println("Sum is " + sum);
     }
 }
 
