00524f2d8ea4864c35ecbc3a7d8382c28e0d0590
tekkukerli <kerlitekku@gmail.com>
2020-10-09 14:59:50 +0300

    ül 36

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,32 @@ import java.util.Scanner; //to read user input
 public class Main {
     public static void main(String[] args) {
        Scanner reader = new Scanner(System.in);
-        System.out.println("Until what? ");
-        int upperNumber = Integer.parseInt(reader.nextLine());
-        int currentNumber = 1;
+        System.out.println("Type numbers: ");
         int sum = 0;
-        while (currentNumber <= upperNumber){
-            sum += currentNumber;
-            currentNumber++;
+        int countAll = 0;
+        int countEven = 0;
+        int countOdd = 0;
+        double avreage = 0.0;
+        while(true){
+            int number = Integer.parseInt(reader.nextLine());
+            if(number == -1){
+                break;
+            }
+            countAll++;
+            if(number % 2 == 0){
+                countEven++;
+            } else {
+                countOdd++;
+            }
+            sum += number;
+            avreage = (double)sum / countAll;
         }
+        System.out.println("Thank you and see you later!");
         System.out.println("Sum is " + sum);
+        System.out.println("How many numbers: " + countAll);
+        System.out.println("How many even numbers: " + countEven);
+        System.out.println("How many odd numbers: " + countOdd);
+        System.out.println("Average" + avreage);
     }
 }
 
