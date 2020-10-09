0a87124c2fbd0afec040031502db74476bc73d63
tekkukerli <kerlitekku@gmail.com>
2020-10-09 15:02:12 +0300

    ül 37

--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,10 @@ import java.util.Scanner; //to read user input
 
 public class Main {
     public static void main(String[] args) {
-       Scanner reader = new Scanner(System.in);
-        System.out.println("Type numbers: ");
-        int sum = 0;
-        int countAll = 0;
-        int countEven = 0;
-        int countOdd = 0;
-        double avreage = 0.0;
-        while(true){
-            int number = Integer.parseInt(reader.nextLine());
-            if(number == -1){
-                break;
-            }
-            countAll++;
-            if(number % 2 == 0){
-                countEven++;
-            } else {
-                countOdd++;
-            }
-            sum += number;
-            avreage = (double)sum / countAll;
-        }
-        System.out.println("Thank you and see you later!");
-        System.out.println("Sum is " + sum);
-        System.out.println("How many numbers: " + countAll);
-        System.out.println("How many even numbers: " + countEven);
-        System.out.println("How many odd numbers: " + countOdd);
-        System.out.println("Average" + avreage);
+       printText();
+    }
+    public static void printText() {
+        System.out.println("In the beginning there were the swamp, the hoe and Java.");
     }
 }
 
