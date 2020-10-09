a7a08620cd5c7e848d2f23cb2a79ab640002b6d0
tekkukerli <kerlitekku@gmail.com>
2020-10-09 14:47:53 +0300

    ül 30

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,14 @@ import java.util.Scanner; //to read user input
 
 public class Main {
     public static void main(String[] args) {
-       int number = 2;
-       while(number <= 100){
-           if(number % 2 == 0){
-               System.out.println(number);
-           }
-           number++;
-       }
+       Scanner reader = new Scanner(System.in);
+        System.out.println("Up to what number");
+        int upperNumber = Integer.parseInt(reader.nextLine());
+        int currentNumber = 1;
+        while (currentNumber <= upperNumber){
+            System.out.println(currentNumber);
+            currentNumber++;
+        }
     }
 }
 
