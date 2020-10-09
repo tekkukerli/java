ff0f20d14cf038926c41f2bd356f1ec4e89078d0
tekkukerli <kerlitekku@gmail.com>
2020-10-09 14:44:31 +0300

    ül 29

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,13 @@ import java.util.Scanner; //to read user input
 
 public class Main {
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in); //loeb andmed
-        int sum = 0;
-        while (true) {
-            int read = Integer.parseInt(reader.nextLine()); //loeb sisendit
-            if (read == 0) {
-                break;
-            }
-
-            // DO SOMETHING HERE
-            sum = sum + read; //sum += read;
-
-            System.out.println("Sum now: " + sum);
-        }
-
-        System.out.println("Sum in the end: " + sum);
+       int number = 2;
+       while(number <= 100){
+           if(number % 2 == 0){
+               System.out.println(number);
+           }
+           number++;
+       }
     }
 }
 
