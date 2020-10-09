2864cf286332a5ede4adc4e5799945b41043c201
tekkukerli <kerlitekku@gmail.com>
2020-10-09 15:27:39 +0300

    ül 41

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,32 @@
 import java.util.Scanner; //to read user input
 
 public class Main {
+    private static boolean drawNumber(int guess, int number, int count) {
+
+        System.out.println(number);
+        if(guess < number) {
+            System.out.println("The number is greater, guesses made: " + count);
+            return true;
+        } else if (guess > number){
+            System.out.println("The number is lesser, guesses made: " + count);
+            return true;
+        } else {
+            System.out.println("Congratulations, your guess is correct");
+            return false;
+        }
+      }
+
     public static void main(String[] args) {
-       printText();
-    }
-    public static void printText() {
-        System.out.println("In the beginning there were the swamp, the hoe and Java.");
+        int number = (int)(Math.random() * 101);
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Guess a number: ");
+        int guess = Integer.parseInt(reader.nextLine());
+        int count = 1;
+        while(drawNumber(guess, number, count)){
+            System.out.println("Guess a number: ");
+            guess = Integer.parseInt(reader.nextLine());
+            count++;
+        }
     }
 }
 
