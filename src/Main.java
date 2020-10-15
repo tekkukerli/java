30e6ecb834e8a792de8fa0ab62cf5e51b9cd372b
tekkukerli <kerlitekku@gmail.com>
2020-10-15 10:49:03 +0300

    ül 62

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,27 @@ import java.util.Collections;
 
 public class Main {
 
-    public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<String>();
-
-        list.add("Hallo");
-        list.add("Ciao");
-        list.add("Hello");
-        System.out.println("There are this many items in the list:");
-        System.out.println(countItems(list));
+    public static void removeLast(ArrayList<String> list) {
+        list.remove(list.size() -1);
     }
-    public static int countItems(ArrayList<String> list) {
-        return list.size();
+
+    public static void main(String[] args) {
+        ArrayList<String> brothers = new ArrayList<String>();
+        brothers.add("Dick");
+        brothers.add("Henry");
+        brothers.add("Michael");
+        brothers.add("Bob");
+
+        System.out.println("brothers:");
+        System.out.println(brothers);
+
+// sorting brothers
+        Collections.sort(brothers);
+
+// removing the last item
+        removeLast(brothers);
+
+        System.out.println(brothers);
     }
 
 }
