1d2a7759045745528a44cbff4663abdf0633fc52
tekkukerli <kerlitekku@gmail.com>
2020-10-15 09:38:37 +0300

    ül 57

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,30 @@
 import java.util.Scanner; //to read user input
+import java.util.ArrayList;
 
 public class Main {
 
-    public static String reverse(String text) {
-        int i = text.length() -1;
-        String reverseText = "";
-        while (i >= 0) {
-            reverseText += text.charAt(i);
-            i--;
-        }
-        return reverseText;
-    }
-
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        ArrayList<String> words = new ArrayList<String>();
 
-        System.out.print("Type in your text: ");
-        String text = reader.nextLine();
+        while(true) {
+            System.out.println("Type a word: ");
+            String word = reader.nextLine();
 
-        System.out.println("In reverse order: " + reverse(text));
+            if(word.isEmpty()) {
+                break;
+            } else {
+                words.add(word);
+            }
+        }
+
+        System.out.println("You typed in following words: ");
+        for (String word : words
+        ) {
+            System.out.println(word);
+        }
+
+
     }
 
 }
