c15ed21bf460d6fbf54ca42c4159cad4e2c561a8
tekkukerli <kerlitekku@gmail.com>
2020-10-15 09:14:47 +0300

    ül 50

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
 import java.util.Scanner; //to read user input
 
 public class Main {
-    public static double average(int number1, int number2, int number3, int number4) {
-        int sum = number1 + number2 + number3 + number4;
-        double average = (double) sum / 4;
-        return average;
-    }
 
     public static void main(String[] args) {
-        double answer = average(4, 3, 6, 1);
-        System.out.println("average: " + answer);
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Type your name: ");
+        String name = reader.nextLine();
+
+        if (name.length() >= 3) {
+            System.out.println("1. character: " + name.charAt(0));
+            System.out.println("2. character: " + name.charAt(1));
+            System.out.println("3. character: " + name.charAt(2));
+        }
+
     }
+
 }
 
