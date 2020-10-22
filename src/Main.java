3d4d477cba044ec03d4facb269bc17dcba0d01c5
tekkukerli <kerlitekku@gmail.com>
2020-10-22 18:08:26 +0300

    ül 76.2

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,14 @@ import java.util.ArrayList;
 public class Main {
 
         public static void main(String[] args) {
-            DecreasingCounter counter = new DecreasingCounter(100);
+                Menu menu = new Menu();
+                menu.addMeal("Hamburger");
+                menu.addMeal("Fish'n'Chips");
+                menu.addMeal("Sauerkraut");
 
-            counter.printValue();
-
-            counter.decrease();
-            counter.printValue();
-
-            counter.decrease();
-            counter.printValue();
-
-            counter.reset();
-            counter.printValue();
-
-            counter.setInitial();
-            counter.printValue();
-
+                menu.printMeals();
         }
+
 
 
 }
