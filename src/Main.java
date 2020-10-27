522cd4575cb0db7c9ed1d3f8bec6baf72811b8b7
tekkukerli <kerlitekku@gmail.com>
2020-10-27 17:41:50 +0200

    ül 79.4

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.util.Scanner; //to read user input
 public class Main {
     public static void main(String[] args) {
         NumberStatistics sum = new NumberStatistics();
+        NumberStatistics even = new NumberStatistics();
+        NumberStatistics odd = new NumberStatistics();
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Insert numbers: ");
@@ -15,11 +17,18 @@ public class Main {
             if(number == -1) {
                 break;
             } else {
-                sum.addNumber(number);
+                if (number % 2 == 0) {
+                    even.addNumber(number);
+                } else {
+                    odd.addNumber(number);
+                }
+
             }
 
         }
         System.out.println("Sum: " + sum.sum());
+        System.out.println("Even sum: " + even.sum());
+        System.out.println("Odd sum: " + odd.sum());
     }
 
 }
