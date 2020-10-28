9ca0c55b885b2bc3200dfcf2a53ef31a7ce923ef
tekkukerli <kerlitekku@gmail.com>
2020-10-28 21:40:58 +0200

    ül 88.2

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,27 @@ import java.util.ArrayList; // import the ArrayList class
 
 public class Main {
     public static void main(String[] args) {
-        Student pekka = new Student("Pekka Mikkola", "013141590");
-        System.out.println("name: " + pekka.getName());
-        System.out.println("studentnumber: " + pekka.getStudentNumber());
-        System.out.println(pekka);
+        ArrayList<Student> students = new ArrayList<Student>();
+        Scanner reader = new Scanner(System.in);
+
+        while (true){
+            System.out.print("name: ");
+            String name = reader.nextLine();
+            if(name.isEmpty()) {
+                break;
+            }
+            System.out.print("studentnumber: ");
+            String studentNumber = reader.nextLine();
+            students.add(new Student(name, studentNumber));
+        }
+
+        for (Student student: students
+        ) {
+            System.out.println(student);
+        }
+
+
+
     }
 }
 
