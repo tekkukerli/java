e1027687cbc22af92bdb214cb97ad6c4499f186f
tekkukerli <kerlitekku@gmail.com>
2020-10-28 21:46:26 +0200

    ül 88.3

--- src/Main.java
+++ src/Main.java
@@ -22,6 +22,16 @@ public class Main {
             System.out.println(student);
         }
 
+        System.out.print("Give search term: ");
+        String searchTerm = reader.nextLine();
+        System.out.println("Result:");
+        for (Student student: students
+        ) {
+            if(student.getName().contains(searchTerm)){
+                System.out.println(student);
+            }
+        }
+
 
 
     }
