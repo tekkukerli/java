bf109461045574a3ea22d00bdd3ff02114135026
tekkukerli <kerlitekku@gmail.com>
2020-11-02 14:57:04 +0200

    ül 105.1

---
+++ src/GuessingGame.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class GuessingGame {
+
+    private Scanner reader;
+
+    public GuessingGame() {
+        this.reader = new Scanner(System.in);
+    }
+
+    public void play(int lowerLimit, int upperLimit) {
+        instructions(upperLimit, lowerLimit);
+
+        // write the game logic here
+    }
+
+    // implement here the methods isGreaterThan and average
+    public boolean isGreaterThan(int value){
+        System.out.println("Is your number greater than " + value + "? (y/n)");
+        if(this.reader.nextLine().equals("y")){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void instructions(int lowerLimit, int upperLimit) {
+        int maxQuestions = howManyTimesHalvable(upperLimit - lowerLimit);
+
+        System.out.println("Think of a number between " + lowerLimit + "..." + upperLimit + ".");
+
+        System.out.println("I promise you that I can guess the number you are thinking of with " + maxQuestions + " questions.");
+        System.out.println("");
+        System.out.println("Next I'll present you with a series of questions. Answer them honestly.");
+        System.out.println("");
+    }
+
+    // a helper method:
+    public static int howManyTimesHalvable(int number) {
+        // we create a base two logarithm  of the given value
+        // Below we swap the base number to base two logarithms!
+        return (int) (Math.log(number) / Math.log(2)) + 1;
+    }
+}
