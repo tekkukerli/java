82c277b6b33cf4ab0e42ad479235c3315828c8cc
tekkukerli <kerlitekku@gmail.com>
2020-11-02 13:20:32 +0200

    ül 98.2

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,11 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] original = {1, 2, 3, 4};
-        int[] copied = copy(original);
-
-        // change the copied
-        copied[0] = 99;
+        int[] reverse = reverseCopy(original);
 
         // print both
-        System.out.println( "original: " + Arrays.toString(original));
-        System.out.println( "copied: " + Arrays.toString(copied));
+        System.out.println( "original: " +Arrays.toString(original));
+        System.out.println( "reversed: " +Arrays.toString(reverse));
     }
 
     public static int[] copy(int[] array) {
@@ -21,6 +18,14 @@ public class Main {
         return newArray;
     }
 
+    public static int[] reverseCopy(int[] array) {
+        int[] newArray = new int[array.length];
+        for(int i = array.length-1, j = 0; i >= 0; i--){
+            newArray[j] = array[i];
+            j++;
+        }
+        return newArray;
+    }
 
 
 }
