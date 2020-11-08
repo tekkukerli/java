4f6fb2277531ac0cc3837e21e9d2e11e946563b1
tekkukerli <kerlitekku@gmail.com>
2020-11-08 20:24:09 +0200

    ül 9.2

---
+++ src/VehicleRegister.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+public class VehicleRegister {
+
+    public HashMap<RegistrationPlate, String> owners;
+
+    public VehicleRegister() {
+        owners = new HashMap<RegistrationPlate, String>();
+    }
+
+    public boolean add(RegistrationPlate plate, String owner) {
+        if (owners.containsKey(plate)) {
+            return false;
+        }
+        owners.put(plate, owner);
+        return true;
+    }
+
+    public String get(RegistrationPlate plate) {
+        if (!owners.containsKey(plate)) {
+            return null;
+        }
+        return owners.get(plate);
+    }
+
+    public boolean delete(RegistrationPlate plate) {
+        if (!owners.containsKey(plate)) {
+            return false;
+        }
+        owners.remove(plate);
+        return true;
+    }
+}
